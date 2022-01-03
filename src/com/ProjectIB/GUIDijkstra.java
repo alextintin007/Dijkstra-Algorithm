fd08bcd242957fcd3efbd764e50651f2076c5250
Xavier Tintin <alextintin007@outlook.com>
2022-01-02 20:27:38 -0500

    Final working project Mark1

--- src/com/ProjectIB/GUIDijkstra.java
+++ src/com/ProjectIB/GUIDijkstra.java
@@ -1,7 +1,13 @@
 package com.ProjectIB;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GUIDijkstra extends JFrame  {
     private JPanel mainPanel;
@@ -10,13 +16,41 @@ public class GUIDijkstra extends JFrame  {
     private JLabel lblTitle;
     private JButton btnEnter;
     private JLabel lblDistance;
+    private JRadioButton rbt0;
+    private JRadioButton rbt1;
+    private JRadioButton rbt2;
+    private JRadioButton rbt3;
+    private JRadioButton rbt4;
+    private JRadioButton rbt5;
+    private JRadioButton rbt6;
+    private JRadioButton rbt8;
+    private JRadioButton rbt7;
+    private JRadioButton rbt9;
+    private JLabel lblArrow1;
+    private JLabel lblArrow2;
+    private JLabel lblArrow4;
+    private JLabel lblArrow3;
+    private JLabel lblArrow5;
+
+    JLabel displayField;
+    ImageIcon image;
 
     public GUIDijkstra(){
         setContentPane(mainPanel);
         setTitle("Dijkstra Algorithm");
-        this.setSize(450,300);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        setSize(650,450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        try{
+            image = new ImageIcon("/Users/alextintin/Downloads/quito.jpg");
+            displayField = new JLabel(image);
+            this.add(displayField);
+        }catch (Exception c){
+            System.out.println("No hay imagen");
+        }
+        setLocationRelativeTo(null);
         setVisible(true);
+        setResizable(false);
+
         btnEnter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -33,10 +67,78 @@ public class GUIDijkstra extends JFrame  {
                 min=solver.dijkstra(0, endNode);
                 System.out.println("Pasos del camino mas corto dentro de nuestro sistema: "+min);
                 lblDistance.setText(Double.toString(min));
+
+                List<Integer> path = new ArrayList<>();
+                path=solver.dijkstraList(0,4);
+                for (int i = 0; i < path.size(); i++) {
+                    System.out.println(path.get(i));
+                }
+                for (int i = 0; i <5; i++) {
+                    switch (path.get(i)){
+                        case 0:
+                            rbt0.setSelected(true);
+                            break;
+                        case 1:
+                            rbt1.setSelected(true);
+                            break;
+                        case 2:
+                            rbt2.setSelected(true);
+                            break;
+                        case 3:
+                            rbt3.setSelected(true);
+                            break;
+                        case 4:
+                            rbt4.setSelected(true);
+                            break;
+                        case 5:
+                            rbt5.setSelected(true);
+                            break;
+                        case 6:
+                            rbt6.setSelected(true);
+                            break;
+                        case 7:
+                            rbt7.setSelected(true);
+                            break;
+                        case 8:
+                            rbt8.setSelected(true);
+                            break;
+                        case 9:
+                            rbt9.setSelected(true);
+                            break;
+                        default: break;
+                    }
+
+                }
             }
         });
     }
     public static void main(String[] args){
         GUIDijkstra myFrame = new GUIDijkstra();
+        Image i = new Image() {
+            @Override
+            public int getWidth(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public int getHeight(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public ImageProducer getSource() {
+                return null;
+            }
+
+            @Override
+            public Graphics getGraphics() {
+                return null;
+            }
+
+            @Override
+            public Object getProperty(String name, ImageObserver observer) {
+                return null;
+            }
+        };
     }
 }
--- src/com/ProjectIB/Main.java
+++ src/com/ProjectIB/Main.java
@@ -19,8 +19,14 @@ public class Main {
 
         min=solver.dijkstra(0, 4);
         System.out.println("Pasos del camino mas corto dentro de nuestro sistema: "+min);
-
+        // A list that defines the exact & ordered path with the minimum distance
+        List<Integer> path = new ArrayList<>();
+        path=solver.dijkstraList(0,4);
+        for (int i = 0; i < path.size(); i++) {
+            System.out.println(path.get(i));
+        }
     }
+
     // An edge class to represent a directed edge
     // between two nodes with a certain cost.
     public static class Edge {
@@ -122,6 +128,7 @@ public class Main {
                     if (!ipq.contains(edge.to)) ipq.insert(edge.to, newDist);
                     else ipq.decrease(edge.to, newDist);
                 }
+                /*
                 if (dist[nodeId] > 3) {
                     List<Integer> path = new ArrayList<>();
                     for (Integer at = end; at != null; at = prev[at]) path.add(at);
@@ -129,7 +136,7 @@ public class Main {
                     for (int i = 0; i < path.size(); i++) {
                         System.out.println(path.get(i));
                     }
-                }
+                }*/
             }
             // Once we've processed the end node we can return early (without
             // necessarily visiting the whole graph) because we know we cannot get a
@@ -138,9 +145,69 @@ public class Main {
             if (nodeId == end) return dist[end];
 
         }
-
         // End node is unreachable.
         return Double.POSITIVE_INFINITY;
+    }
+
+    public List dijkstraList(int start, int end) {
+
+        // Keep an Indexed Priority Queue (ipq) of the next most promising node
+        // to visit.
+        int degree = edgeCount / n;
+        MinIndexedDHeap<Double> ipq = new MinIndexedDHeap<>(degree, n);
+        ipq.insert(start, 0.0);
+
+        // Maintain an array of the minimum distance to each node.
+        dist = new double[n];
+        Arrays.fill(dist, Double.POSITIVE_INFINITY);
+        dist[start] = 0.0;
+        boolean[] visited = new boolean[n];
+        prev = new Integer[n];
+
+        while (!ipq.isEmpty()) {
+            int nodeId = ipq.peekMinKeyIndex();
+            visited[nodeId] = true;
+            double minValue = ipq.pollMinValue();
+
+            // We already found a better path before we got to
+            // processing this node so we can ignore it.
+            if (minValue > dist[nodeId]) continue;
+
+            for (Edge edge : graph.get(nodeId)) {
+
+                // We cannot get a shorter path by revisiting
+                // a node we have already visited before.
+                if (visited[edge.to]) continue;
+
+                // Relax edge by updating minimum cost if applicable.
+                double newDist = dist[nodeId] + edge.cost;
+                if (newDist < dist[edge.to]) {
+                    prev[edge.to] = nodeId;
+                    dist[edge.to] = newDist;
+
+                    // Insert the cost of going to a node for the first time in the PQ,
+                    // or try and update it to a better value by calling decrease.
+                    if (!ipq.contains(edge.to)) ipq.insert(edge.to, newDist);
+                    else ipq.decrease(edge.to, newDist);
+                }
+                if (dist[nodeId] > 3) {
+                    List<Integer> path = new ArrayList<>();
+                    for (Integer at = end; at != null; at = prev[at]) path.add(at);
+                    Collections.reverse(path);
+
+                    return path;
+                }
+            }
+            // Once we've processed the end node we can return early (without
+            // necessarily visiting the whole graph) because we know we cannot get a
+            // shorter path by routing through any other nodes since Dijkstra's is
+            // greedy and there are no negative edge weights.
+            //if (nodeId == end) return dist[end];
+
+        }
+
+        // End node is unreachable.
+        return null;
     }
 
     /**
